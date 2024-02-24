1164666c50a8a389b73ab9558dce34e02c5a40db
dns147 <rarden@ya.ru>
2024-02-24 18:01:01 +0300

    refactor: static server

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import { handleMessages } from "handleMessages";
+import { httpServer } from "http_server";
 import { WebSocketServer } from "ws";
+
+const HTTP_PORT = 8181;
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+httpServer.listen(HTTP_PORT);
 
 export const wsServer = new WebSocketServer({ port: 3000 });
 
